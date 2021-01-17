b02ec83c90c5f5286fcdc00d2469c17a1ea707ae
zhaojinhui <zhaojinhui@RedFoxAtAsleep-3.local>
2021-01-17 18:21:20 +0800

    修改vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  lintOnSave: false
-}
-
-module.exports = {
-  publicPath: '/YourRepoName/'
-}
-module.exports = {
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       // 为生产环境修改配置...
