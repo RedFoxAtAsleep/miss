4212e3c361018aad2d8db38b8bf01a97593117e4
zhaojinhui <zhaojinhui@RedFoxAtAsleep-3.local>
2021-01-17 17:34:18 +0800

    添加空白.env文件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   lintOnSave: false
 }
+
+module.exports = {
+  publicPath: '/YourRepoName/'
+}
+module.exports = {
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+      return {
+        publicPath: '/miss/'
+      }
+    } else {
+      // 为开发环境修改配置...
+
+    }
+  }
+}
