13929c0e57f079b8fff464728e2bf57536ca719f
zhaojinhui <zhaojinhui@RedFoxAtAsleep-3.local>
2021-01-17 23:09:24 +0800

    Github Page自定义域名配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const config = {
+  'GHPAGE_CUSTOM_DOMAIN':{
+    publicPath: '/',
+    outputDir: 'dist_for_ghpage',
+    //assetsDir:'assets',//静态资源目录(js,css,img,fonts)这些文件都可以写里面
+    lintOnSave:false,//是否开启eslint保存检测 ,它的有效值为 true || false || 'error'\
+  },
   'GHPAGE':{
     publicPath: '/miss/',
     outputDir: 'dist_for_ghpage',
@@ -10,10 +16,12 @@ const config = {
     outputDir: 'dist_for_django',  // 相对于根目录
     indexPath: 'templates/index.html',  // 相对于outputDir
     assetsDir: 'static',  // 相对于outputDir，js、css、img、fonts
+    lintOnSave:false,
   },
   'HTMLPK':{
     publicPath: './',
     outputDir: 'dist_for_htmlpk',
+    lintOnSave:false,
   }
 }  // GHPAGE DJANGO HTMLPK
 module.exports = config[process.env.BUILD_FOR]
