516c23e21cffe3390504ce65dfc541c8de87d67f
zhaojinhui <zhaojinhui@RedFoxAtAsleep-3.local>
2021-01-17 18:15:12 +0800

    修改vue.config.js和package.json

--- vue.config.js
+++ vue.config.js
@@ -9,12 +9,16 @@ module.exports = {
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       // 为生产环境修改配置...
-      return {
-        publicPath: '/miss/'
+      if (process.env.BUILD_FOR_GHPAGE === 'on'){
+        return {
+          publicPath: '/miss/',
+          outputDir: 'dist',  // 相对于根目录
+        }
       }
+      return {}
     } else {
       // 为开发环境修改配置...
-
+      return {}
     }
   }
 }
